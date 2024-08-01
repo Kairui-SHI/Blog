363fc6590427ddf0bb82e1fb758544dbb2d3f631
Kairui-SHI <2977493767@qq.com>
2024-07-31 21:24:45 -0400

    update 07/31/2024

--- src/config.ts
+++ src/config.ts
@@ -34,7 +34,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/Kairui-SHI',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
